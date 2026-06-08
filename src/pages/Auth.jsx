6fe90271a59e475731402f1accd18b74f947a9a2
Ibrahim Programmer <hemazogy@gmail.com>
2026-06-08 11:38:25 +0300

    product details

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,6 +1,6 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { AuthContext } from "../context/AuthContext";
+import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 function Auth() {
@@ -9,7 +9,7 @@ function Auth() {
 
   const navigate = useNavigate();
 
-  const { signUp, user, logout, login } = useContext(AuthContext);
+  const { signUp, login } = useAuth();
 
   const {
     register,
@@ -31,15 +31,12 @@ function Auth() {
     } else {
       setError(result.error);
     }
-    console.log(result);
   }
 
   return (
     <div className="page">
       <div className="container">
         <div className="auth-container">
-          {user && <p>User logged in: {user.email}</p>}
-          <button onClick={logout}>Logout</button>
           <h1 className="page-title">
             {mode === "signup" ? "Sing Up" : "Login"}
           </h1>
